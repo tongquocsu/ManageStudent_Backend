cfa1e3bea40f9ce8268de302576073e0ed79a0f1
minktruong1 <minhpro207@yahoo.com>
2023-06-28 17:30:29 +0700

    Import cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import schoolRoutes from "./routes/schoolRoute.js";
@@ -23,6 +24,7 @@ connectDB();
 const app = express();
 
 //middleWares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
